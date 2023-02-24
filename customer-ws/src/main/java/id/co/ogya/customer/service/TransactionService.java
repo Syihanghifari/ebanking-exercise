32a42aeadb1f8b7854d29a729bf632584acc7354
Syihanghifari <syihanghifari27@gmail.com>
2023-02-24 20:46:55 +0700

    bisa

--- customer-ws/src/main/java/id/co/ogya/customer/service/TransactionService.java
+++ customer-ws/src/main/java/id/co/ogya/customer/service/TransactionService.java
@@ -3,12 +3,14 @@ package id.co.ogya.customer.service;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-import id.co.ogya.customer.request.InquiryBalanceRequest;
-import id.co.ogya.customer.request.TransferRequest;
-import id.co.ogya.customer.response.InquiryBalanceResponse;
-import id.co.ogya.customer.response.TransferResponse;
+
+import id.co.ogya.ebanking.ejb.request.TransferRequest;
+
+import id.co.ogya.ebanking.ejb.response.TransferResponse;
 import id.co.ogya.ebanking.ejb.EbankingService;
 import id.co.ogya.ebanking.ejb.util.ServiceFactory;
+import id.co.ogya.ebanking.ejb.response.InquiryBalanceResponse;
+import id.co.ogya.ebanking.ejb.request.InquiryBalanceRequest;
 
 @WebService
 public class TransactionService {
@@ -16,8 +18,8 @@ public class TransactionService {
 	
 	
 	@WebMethod
-	public id.co.ogya.ebanking.ejb.response.InquiryBalanceResponse cekSaldo(id.co.ogya.ebanking.ejb.request.InquiryBalanceRequest inquiryBalanceRequest) throws InvalidInputException {
-		id.co.ogya.ebanking.ejb.response.InquiryBalanceResponse response = new id.co.ogya.ebanking.ejb.response.InquiryBalanceResponse();
+	public InquiryBalanceResponse cekSaldo(InquiryBalanceRequest inquiryBalanceRequest) throws InvalidInputException {
+		InquiryBalanceResponse response = new InquiryBalanceResponse();
 		if(inquiryBalanceRequest.getAccountNo().equals(null)) {
 			throw new InvalidInputException("Field <account no> harus diisi", " OGYA-999");
 		}else {
@@ -31,8 +33,7 @@ public class TransactionService {
 				boolean isAbleToConnect = ebankingService.isConnected();
 				System.out.println("is Able to connect " + isAbleToConnect);
 				response = ebankingService.cekSaldo(inquiryBalanceRequest);
-				response.setErrorCode("OGYA-000");
-				response.setErrorMessage("Transaksi berhasil");
+				
 			}catch(Exception e) {
 				throw new InvalidInputException("error dari backend", " OGYA-999");
 			}
@@ -50,12 +51,14 @@ public class TransactionService {
 		} else if (transferRequest.getAmount().equals(null) || transferRequest.getAmount().equals(0L)) {
 			throw new InvalidInputException("Field <amount> harus diisi", " OGYA-999");
 		} else {
+			String simpleDataSourceAccessJNDIName =
+					"EbankingServiceImpl#id.co.ogya.ebanking.ejb.EbankingService";
 			try {
-
-				// callservice
-				response.setErrorCode("OGYA-000");
-				response.setErrorMessage("Transaksi berhasil");
-				response.setReferenceNumber("ini");
+				ServiceFactory serviceFactory = new ServiceFactory();
+				EbankingService ebankingService = (EbankingService) serviceFactory.getService(simpleDataSourceAccessJNDIName);
+				boolean isAbleToConnect = ebankingService.isConnected();
+				System.out.println("is Able to connect " + isAbleToConnect);
+				response = ebankingService.kirim(transferRequest);
 			} catch (Exception e) {
 				throw new InvalidInputException("error dari backend", " OGYA-999");
 			}
